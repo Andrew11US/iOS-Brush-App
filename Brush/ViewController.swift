4ae8a0580957462eb00c8538a91a74d8ff84c356
Andrii Halabuda <losAngelesBoy11@gmail.com>
2017-02-07 18:50:53 +0200

    Initial Commit

---
+++ Brush/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Brush
+//
+//  Created by Andrew Foster on 2/7/17.
+//  Copyright © 2017 Andrii Halabuda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
